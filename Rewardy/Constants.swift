dc61c2eb909f93f91761131f5ba09ef05a8354ec
NoraC45 <yjjslsyq@gmail.com>
2023-02-09 22:29:16 -0500

    updated categories in the current season

--- Rewardy/Constants.swift
+++ Rewardy/Constants.swift
@@ -8,8 +8,8 @@
 struct K {
     static let cellIdentifier = "customCell"
     static let cardData = [
-        Card(name: "Discover It", seasonBool: true, reward: "5% on Restaurant, Paypal; 1% others"),
-        Card(name: "Chase Freedom", seasonBool: true, reward: "5% on Gas Station, Live Entertainment; 1% others"),
-        Card(name: "Amex Express", seasonBool: false, reward: "3% Supermarket; 2% Department Store; 1% others")
+        Card(name: "Discover It", seasonBool: true, reward: "5% on Grocery Stores, Drug Stores, and Select Streaming Services; 1% others"),
+        Card(name: "Chase Freedom", seasonBool: true, reward: "5% on Grocery Stores (excluding Walmart), Target, Fitness Clubs & Gym Memberships; 1% others"),
+        Card(name: "Amex Express", seasonBool: false, reward: "3% Supermarket, Online Retail, Gas (each up to $6,000/year); 1% others")
     ]
 }
